5e62b0c111036acbbf3ec268e9542d0be76d7f22
ark1210 <ark1210@gmail.com>
2022-09-07 10:42:58 +0900

    Wordversion2

--- WordMasterProject/src/WordCRUD.java
+++ WordMasterProject/src/WordCRUD.java
@@ -1,12 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordCRUD implements ICRUD {
-
+	ArrayList<Word> list;
+	Scanner s;
+	
+	WordCRUD(Scanner s){
+		list = new ArrayList<>();
+		this.s=s;
+	}
 	@Override
 	public Object add() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+		int level = s.nextInt();
+		String word = s.next();
+		
+		System.out.print("뜻 입력 : ");
+		String meaning = s.nextLine();
+		//차고 진입로
+		return new Word(0, level, word, meaning);
 	}
-
+public void addWord() {
+	Word one = (Word)add();
+	list.add(one);
+	System.out.println("새 단어가 단어장에 추가되었습니다. ");
+}
 	@Override
 	public int update(Object obj) {
 		// TODO Auto-generated method stub
